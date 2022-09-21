ffc6b8c082317dba36180620e5e10574c7947b7f
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2022-09-21 17:25:39 -0500

    Add files via upload
    More work on mobile navigation.

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
 const menuToggle = document.getElementById("hamburgerIcon");
+let mobileLinks = document.querySelectorAll(".mobile-nav-item");
 
+console.log(mobileLinks);
 menuToggle.addEventListener('click', () => {
-    ;
+   mobileLinks.forEach(element => {
+    mobileLinks[element].classList.add("active"); 
+   });
 });
