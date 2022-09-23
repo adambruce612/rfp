0843cc49ce7750f9f6d1f1d7ecc5143f3115e3f5
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2022-09-23 16:26:10 -0500

    Add files via upload
    Re-working of menu, needs updating still.

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
 const menuToggle = document.getElementById("hamburgerIcon");
-let mobileLinks = document.querySelector(".mobile-nav-items");
+let mobileLinks = document.querySelector(".mobile-nav-item");
 
 menuToggle.addEventListener('click', () => {
+   if (mobileLinks.style.display === "none") {
    mobileLinks.style.display = "block";
+   } else {
+      mobileLinks.style.display = "none";
+   }
 });
